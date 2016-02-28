bc821dc99f7ee9eb36130747934742eb7564d98b
Andrew Hart <andrew@pogoseat.com>
2016-02-28 08:18:29 -0800

    Return an object in module exports

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var rest = require('./rest.js'),
     ws = require('./ws.js');
 
-var BFX = function(api_key, api_secret){
-    this.api_key = api_key;
-    this.api_secret = api_secret;
-    this.rest = new rest(this.api_key, this.api_secret);
-    this.ws = new ws(this.api_key, this.api_secret);
+var BFX = function(api_key, api_secret) {
+    return {
+        api_key: api_key,
+        api_secret: api_secret,
+        rest: new rest(api_key, api_secret),
+        this: new ws(api_key, api_secret)
+    }
 };
 
 module.exports = BFX;
